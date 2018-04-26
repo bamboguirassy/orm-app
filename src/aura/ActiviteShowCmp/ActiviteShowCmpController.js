ecdf8d21d3778ea1c0218c0441d732b7a653fcdd
davidmignane <diopdavid94@gmail.com>
2018-04-26 11:55:27 +0100

    save to server activiteShow;risqueEntiteList

--- src/aura/ActiviteShowCmp/ActiviteShowCmpController.js
+++ src/aura/ActiviteShowCmp/ActiviteShowCmpController.js
@@ -6,4 +6,48 @@
         });
         evt.fire();
     },
+    registerEdit : function(component, event, helper) {
+		// deployer evenement qu'on veut modifier un risque
+		var item = component.get('v.item');
+		var evt = $A.get("e.c:eventEditActiviteClicked");
+		evt.setParams({
+			"Id" : item.Id
+		});
+		evt.fire();
+	},
+	confirmDeletion : function(component, event, helper) {
+
+		var self = this; // safe reference
+
+		itemToRemove = component.get('v.item');
+		if (itemToRemove) {
+			helper.showSpinner(component);
+			var removeAction = component.get("c.remove");
+			removeAction.setParam("item", itemToRemove);
+			removeAction.setCallback(this, function(response) {
+				var deleteState = response.getState();
+
+				if (deleteState == "SUCCESS") {
+					helper.hideSpinner(component);
+					helper.closeDeleteConfirmationModel(component);
+					helper.showToast('Success', itemToRemove.Name
+							+ " supprimé avec succés ", 'success');
+					var evt = $A.get("e.c:navigateToPlanActionShowShow");
+					evt.setParams({
+						"Id" : component.get('v.item').plan_Action__c
+					});
+					evt.fire();
+				} else {
+					helper.showToast('Error', "Echec suppression "
+							+ itemToRemove.Name, 'error');
+				}
+
+			});
+			$A.enqueueAction(removeAction);
+		} else {
+			helper.showToast('Warning',
+					"Aucun élement selectionné pour la suppression !",
+					'warning');
+		}
+	},
 })
