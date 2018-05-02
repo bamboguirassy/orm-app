662c02cbbed441cc228b91150e58b3b329efc35f
davidmignane <diopdavid94@gmail.com>
2018-05-02 10:06:29 +0100

    internation.... ProcessusSortie*Cmp

--- src/aura/ProcessusSortieNewCmp/ProcessusSortieNewCmpController.js
+++ src/aura/ProcessusSortieNewCmp/ProcessusSortieNewCmpController.js
@@ -1,5 +1,6 @@
 ({
     doInit : function(component, event, helper) {
+    	helper.getFieldLabels(component, event);
 		//refresh MesurePrevention entité List
 		var action = component.get("c.getProcessus");
         action.setParam('entite', component.get('v.processus').proprietaire__c);
