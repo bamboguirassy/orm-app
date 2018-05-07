e6acaa6d1ffe8520848e43369cd6ce5bdf25906f
ngomsalimata <salifadou@gmail.com>
2018-05-07 11:28:52 +0100

    correction internationalisation

--- src/aura/ElementSortieListCmp/ElementSortieListCmpController.js
+++ src/aura/ElementSortieListCmp/ElementSortieListCmpController.js
@@ -1,6 +1,7 @@
 ({
     doInit: function(component, event, helper) {
         helper.refreshList(component, event);
+        helper.getFieldLabels(component, event);
     },
     registerEdit : function(component, event, helper) {
 		// deployer evenement qu'on veut modifier un risque
