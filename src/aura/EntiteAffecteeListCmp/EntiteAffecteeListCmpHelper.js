670e5bf1daac79d97a8e7eb7c93a314b2d55c855
davidmignane <diopdavid94@gmail.com>
2018-05-15 17:01:38 +0000

    next and previous

--- src/aura/EntiteAffecteeListCmp/EntiteAffecteeListCmpHelper.js
+++ src/aura/EntiteAffecteeListCmp/EntiteAffecteeListCmpHelper.js
@@ -1,20 +1,28 @@
-({
-	refreshList : function(component, event) {
-		var action = component.get("c.findAll");
+({		 refreshList : function(component, event) {
+		var action = component.get('c.findAll');
 		action.setParam("risqueEntite", component.get('v.risqueEntite'));
-		action
-				.setCallback(
-						this,
-						function(response) {
-							var state = response.getState();
-							if (state == "SUCCESS") {
-								component.set("v.items", response
-										.getReturnValue());
-							} else {
-								alert("Impossible de recuperer la liste des entités affectées dans entité affectée");
-							}
-
-						});
-		$A.enqueueAction(action);
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === 'SUCCESS' && component.isValid()){
+                var pageSize = component.get("v.pageSize");
+                // hold all the records into an attribute named "items"
+                component.set('v.items', response.getReturnValue());
+                // get size of all the records and then hold into an attribute "totalRecords"
+                component.set("v.totalRecords", component.get("v.items").length);
+                // set star as 0
+                component.set("v.startPage",0);
+                
+                component.set("v.endPage",pageSize-1);
+                var PaginationList = [];
+                for(var i=0; i< pageSize; i++){
+                    if(component.get("v.items").length> i)
+                        PaginationList.push(response.getReturnValue()[i]);    
+                }
+                component.set('v.PaginationList', PaginationList);
+            }else{
+                alert('ERROR');
+            }
+        });
+        $A.enqueueAction(action);
 	}
 })
