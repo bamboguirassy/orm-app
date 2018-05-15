94943a967e0084c6347f31771098aadee286665e
Dibocor <dibocorng@gmail.com>
2018-05-15 15:56:42 +0200

    Pagination du composant PSListCmp

--- src/aura/ElementSortieListCmp/ElementSortieListCmpHelper.js
+++ src/aura/ElementSortieListCmp/ElementSortieListCmpHelper.js
@@ -9,8 +9,21 @@
                     function(response) {
                         var state = response.getState();
                         if (state == "SUCCESS") {
-                            component.set("v.items", response
-                                .getReturnValue());
+                            var pageSize = component.get("v.pageSize");
+				                // hold all the records into an attribute named "items"
+				                component.set('v.items', response.getReturnValue());
+				                // get size of all the records and then hold into an attribute "totalRecords"
+				                component.set("v.totalRecords", component.get("v.items").length);
+				                // set star as 0
+				                component.set("v.startPage",0);
+				                
+				                component.set("v.endPage",pageSize-1);
+				                var PaginationList = [];
+				                for(var i=0; i< pageSize; i++){
+				                    if(component.get("v.items").length> i)
+				                        PaginationList.push(response.getReturnValue()[i]);    
+				                }
+				                component.set('v.PaginationList', PaginationList);
                         } else {
                             alert("Impossible de recuperer la liste des entités sources");
                         }
