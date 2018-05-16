1639741e51af1a444a9cef1a87d0e71f9dc7a6a6
layepythagore <layepythagore.91@gmail.com>
2018-05-16 10:42:46 +0000

    paginate EvidenceListByActivityCmp

--- src/aura/EvidenceListByActivityCmp/EvidenceListByActivityCmpHelper.js
+++ src/aura/EvidenceListByActivityCmp/EvidenceListByActivityCmpHelper.js
@@ -1,21 +1,54 @@
 ({
-refreshList : function(component, event) {
+	refreshList : function(component, event) {
 		var action = component.get("c.findAllContentDocument");
 		action.setParams({
-		'activite':component.get('v.activite')
+			'activite' : component.get('v.activite')
 		});
-		
-		action.setCallback(this, function(response) {
-			var state = response.getState();
-			if (state == "SUCCESS") {
-					component.set("v.items", response.getReturnValue());
-//				$A.enqueueAction(component.get('c.applyDatatable'));
-				
-			} else {
-				alert("Une erreur est survenue lors de la récupération de la liste des pieces jointes");
-			}
 
-		});
+		action
+				.setCallback(
+						this,
+						function(response) {
+							/*var state = response.getState();
+							if (state == "SUCCESS") {
+								component.set("v.items", response
+										.getReturnValue());
+								// $A.enqueueAction(component.get('c.applyDatatable'));
+
+							} else {
+								alert("Une erreur est survenue lors de la récupération de la liste des pieces jointes");
+							}*/
+							
+							var state = response.getState();
+				            if(state === 'SUCCESS' && component.isValid()){
+				                var pageSize = component.get("v.pageSize");
+				                // hold all the records into an attribute named "items"
+				                component.set('v.items', response.getReturnValue());
+				                // get size of all the records and then hold into an attribute "totalRecords"
+				                component.set("v.totalRecords", component.get("v.items").length);
+				                // set star as 0
+				                component.set("v.startPage",0);
+				                
+				                var totalRecords = component.get("v.items").length;
+							    //var div = Math.trunc(totalRecords / pageSize);
+		                        if (totalRecords === pageSize){
+		                          component.set("v.hideNext", true);
+		                          component.set("v.endPage", pageSize - 1);		                          
+		                        } else {
+		                          component.set("v.hideNext", false);
+		                          component.set("v.endPage", pageSize - 1);		                          
+		                        }
+				                var PaginationList = [];
+				                for(var i=0; i< pageSize; i++){
+				                    if(component.get("v.items").length> i)
+				                        PaginationList.push(response.getReturnValue()[i]);    
+				                }
+				                component.set('v.PaginationList', PaginationList);
+				            } else	{
+				                alert('ERROR');
+				            }
+
+						});
 		$A.enqueueAction(action);
 	}
 
