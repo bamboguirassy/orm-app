83a79d7ddc13e5fad0557b1f301641d4ee5b3e84
Dibocor <dibocorng@gmail.com>
2018-05-16 13:32:21 +0200

    pagination RisqueListCmp

--- src/aura/RisqueListCmp/RisqueListCmpHelper.js
+++ src/aura/RisqueListCmp/RisqueListCmpHelper.js
@@ -27,7 +27,16 @@
                 // set star as 0
                 component.set("v.startPage",0);
                 
-                component.set("v.endPage",pageSize-1);
+                var totalRecords = component
+				.get("v.items").length;
+			    //var div = Math.trunc(totalRecords / pageSize);
+                if(totalRecords === pageSize){
+                  component.set("v.hideNext", true);
+                  component.set("v.endPage", pageSize - 1);
+                }else{
+                  component.set("v.hideNext", false);
+                  component.set("v.endPage", pageSize - 1);
+                }
                 var PaginationList = [];
                 for(var i=0; i< pageSize; i++){
                     if(component.get("v.items").length> i)
