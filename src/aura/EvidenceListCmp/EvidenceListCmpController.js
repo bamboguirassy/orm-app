bfa4590a1e59d841995b93a92caedb26f56a1491
layepythagore <layepythagore.91@gmail.com>
2018-05-22 11:12:59 +0000

    move abstractList

--- src/aura/EvidenceListCmp/EvidenceListCmpController.js
+++ src/aura/EvidenceListCmp/EvidenceListCmpController.js
@@ -4,34 +4,34 @@
 			helper.refreshList(component, event);
 		}
 		helper.getFieldLabels(component, event);
-		
+
 	},
-	showformfileUploadNew:function(component,event,helper){
-	component.set('v.isOpenfileUploadNewCmp',true);
-	var eltselected= helper.getSelectedItem(component,event);
-	component.set('v.parentId',eltselected.Id);
-},
-showListfileByMesure : function(component,event,helper){
-		component.set('v.isOpenListfileUploadCmp',true);
-		var eltselected= helper.getSelectedItem(component,event);
-		component.set('v.parentId',eltselected.Id);
-   // deploiement de l'evenement
-	var evt = $A.get("e.c:eventListFileUploadClicked");
-    evt.setParams({
+	showformfileUploadNew : function(component, event, helper) {
+		component.set('v.isOpenfileUploadNewCmp', true);
+		var eltselected = helper.getSelectedItem(component, event);
+		component.set('v.parentId', eltselected.Id);
+	},
+	showListfileByMesure : function(component, event, helper) {
+		component.set('v.isOpenListfileUploadCmp', true);
+		var eltselected = helper.getSelectedItem(component, event);
+		component.set('v.parentId', eltselected.Id);
+		// deploiement de l'evenement
+		var evt = $A.get("e.c:eventListFileUploadClicked");
+		evt.setParams({
 			"Id" : eltselected.Id
 		});
-  evt.fire();
-},
+		evt.fire();
+	},
 	regEdit : function(component, event, helper) {
-	
+
 		// deployer evenement qu'on veut modifier un impact
-		var item=helper.getSelectedItem(component, event);
+		var item = helper.getSelectedItem(component, event);
 		var evt = $A.get("e.c:eventEditEvidenceClicked");
 		evt.setParams({
 			"Id" : item.Id
 		});
 		evt.fire();
-		
+
 	}
 
 })
